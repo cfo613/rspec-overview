6920a4ae7d45b94326774adf21691d8df20f19e8
Cfo613 <cforan13@gmail.com>
2015-02-22 13:49:16 -0500

    second commit

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,7 +1,26 @@
 class Bank
   attr_accessor :name
+  attr_reader :accounts
 
   def initialize(name)
     @name = name
+    @accounts = {}
   end
+
+  def create_account(name, deposit)
+    @accounts[name] = deposit
+  end
+
+  def deposit(name, amount)
+    @accounts[name] += amount
+  end
+
+  def withdraw(name, amount)
+    @accounts[name] -= amount
+  end
+
+  def balance(name)
+    @accounts[name]
+  end
+
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -7,6 +7,10 @@ describe Bank do
     it "should create a Bank object" do
       expect(bank).to_not eq nil #eq is short for equal (essentially saying the bank object should not be empty)
     end
+
+    it "should have no accounts" do
+      expect(bank.accounts.count).to eq 0
+    end
   end
 
   describe "#name" do
@@ -19,6 +23,29 @@ describe Bank do
     it "should create an account" do
       bank.create_account("Bob", 200)
       expect(bank.accounts["Bob"]).to eq 200
+    end
+  end
+
+  describe "#depost" do
+    it "should deposit money from the client into an account" do
+      bank.create_account("Bob", 200)
+      bank.deposit("Bob", 300)
+      expect(bank.accounts["Bob"]).to eq 500
+    end
+  end
+
+  describe "#balance" do
+    it "should return the balance for the client" do
+      bank.create_account("Bob", 200)
+      expect(bank.balance("Bob")).to eq 200
+    end
+  end
+
+  describe "#withdraw" do
+    it "should subtract money from the account" do
+      bank.create_account("Bob", 200)
+      bank.withdraw("Bob", 50)
+      expect(bank.balance("Bob")).to eq 150
     end
   end
 
